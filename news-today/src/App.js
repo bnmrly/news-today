b0f4b5dd12c5a4fb56d114c29038a90d3e7318b9
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-13 12:10:06 +0100

    basic page layout set up, not split into components yet

--- news-today/src/App.js
+++ news-today/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="App-container">
+        <header className="header">
+          <div className="header-title">
+            <a href="index.html" className="site-title">
+              News Today
+            </a>
+          </div>
+          <nav className="nav">
+            <button type="submit">Coding</button>
+            <button type="submit">Cooking</button>
+            <button type="submit">Football</button>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="main-container">
+          <article className="article-container">
+            <ul>
+              <li>Article here</li>
+              <li>Article here</li>
+              <li>Article here</li>
+              <li>Article here</li>
+            </ul>
+          </article>
+        </main>
+        <footer className="footer">Footer here</footer>
       </div>
     );
   }
