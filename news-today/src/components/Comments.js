6369b1740717ad52014211df22265d81a25d8641
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-14 17:50:24 +0100

    comments set up

--- news-today/src/components/Comments.js
+++ news-today/src/components/Comments.js
@@ -1,1 +1,38 @@
-// import React from 'react';
+import React, { Component } from 'react';
+import * as api from '../api';
+import './Comments.css';
+
+class Comments extends Component {
+  state = {
+    comments: []
+  };
+
+  componentDidMount = () => {
+    api.fetchComments(this.props.articleId).then(data => {
+      this.setState({ comments: data.comments });
+    });
+  };
+
+  render() {
+    const { comments } = this.state;
+    console.log(comments, '*******');
+
+    return (
+      <section className="comments-list">
+        <h3>User Comments:</h3>
+        {comments.map(comment => {
+          return (
+            <section className="comment-section" key={comment._id}>
+              <p>{comment.body}</p>
+              <p>Votes: {comment.votes}</p>
+              <p>Created at: {comment.created_at}</p>
+              <p>Posted by: {comment.created_by.username}</p>
+            </section>
+          );
+        })}
+      </section>
+    );
+  }
+}
+
+export default Comments;
