e2eafb83bacda780c2eb02cea786a3e8fcca06c0
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-15 23:09:40 +0100

    add topic logos

--- news-today/src/components/Topic.js
+++ news-today/src/components/Topic.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import * as api from '../api';
 import ArticlesList from './ArticlesList';
+import './Topic.css';
+import codingArticlesImage from '../images/coding-articles.png';
+import cookingArticlesImage from '../images/cooking-articles.png';
+import footballArticlesImage from '../images/football-articles.png';
 
 class Topic extends React.Component {
   state = {
@@ -27,11 +31,33 @@ class Topic extends React.Component {
     }
   }
 
+  // check on an else condition for ternary
   render() {
     const topic = this.props.match.params.topic;
+    if (topic === 'football') {
+      console.log("it's football");
+    } else console.log("it's another topic");
     return (
       <div>
-        <h2>{topic} Articles:</h2>
+        {topic === 'coding' ? (
+          <img
+            className="topic-image"
+            src={codingArticlesImage}
+            alt="coding Articles heading"
+          />
+        ) : topic === 'cooking' ? (
+          <img
+            className="topic-image"
+            src={cookingArticlesImage}
+            alt="cooking Articles heading"
+          />
+        ) : (
+          <img
+            className="topic-image"
+            src={footballArticlesImage}
+            alt="football Articles heading"
+          />
+        )}
         <ArticlesList articleData={this.state.articleData} />
       </div>
     );
