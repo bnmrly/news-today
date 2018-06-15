57489fe54fcb970571bc84fe2f83484c8a09ccc8
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-15 11:10:48 +0100

    resolve articles by topic

--- news-today/src/api.js
+++ news-today/src/api.js
@@ -10,3 +10,13 @@ export const fetchArticle = async article_id => {
   const { data } = await axios.get(`${url}/articles/${article_id}`);
   return data;
 };
+
+export const fetchComments = async article_id => {
+  const { data } = await axios.get(`${url}/articles/${article_id}/comments`);
+  return data;
+};
+
+export const fetchArticlesByTopic = async topic => {
+  const { data } = await axios.get(`${url}/topics/${topic}/articles`);
+  return data;
+};
---
+++ news-today/src/components/Topic.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import * as api from '../api';
+import ArticlesList from './ArticlesList';
+
+class Topic extends React.Component {
+  state = {
+    articleData: []
+  };
+
+  componentDidMount = () => {
+    const topic = this.props.match.params.topic;
+    api.fetchArticlesByTopic(topic).then(data => {
+      this.setState({
+        articleData: data
+      });
+    });
+  };
+
+  componentDidUpdate(prevProps) {
+    const topic = this.props.match.params.topic;
+    if (this.props.match.params.topic !== prevProps.match.params.topic) {
+      api.fetchArticlesByTopic(topic).then(data => {
+        this.setState({
+          articleData: data
+        });
+      });
+    }
+  }
+
+  render() {
+    const topic = this.props.match.params.topic;
+    return (
+      <div>
+        <h2>Correct topic goes here: {topic}</h2>
+        <ArticlesList articleData={this.state.articleData} />
+      </div>
+    );
+  }
+}
+
+export default Topic;
