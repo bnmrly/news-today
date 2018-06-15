185c4bb88cede44b20cd6c6cac6b527dc16418ed
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-15 15:28:55 +0100

    fix logo filepath issue

--- news-today/src/components/Header.js
+++ news-today/src/components/Header.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
+import logo from '../images/logo.png';
 
 class SiteTitle extends Component {
   render() {
     return (
       <div className="header-title">
         <NavLink className="header-title-link" to="/">
-          {/* <img
-            className="logo"
-            src="https://fontmeme.com/permalink/180614/fa7002ea0aaca91c89a03015b0fc30f3.png"
-            alt=""
-          /> */}
-          <img className="site-logo" src="site-logo.png" alt="site logo" />
+          <img className="site-logo" src={logo} alt="site logo" />
         </NavLink>
       </div>
     );
