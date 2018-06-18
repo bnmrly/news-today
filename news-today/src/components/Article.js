b4ba7ad87300bb3ca911605b9ba3c670cb2ab6a5
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-18 12:12:33 +0100

    article page styling

--- news-today/src/components/Article.js
+++ news-today/src/components/Article.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import * as api from '../api';
+import url from '../api';
 import Comments from './Comments';
+import { Link } from 'react-router-dom';
 import './Article.css';
+import voteUp from '../images/vote-up.png';
+import voteDown from '../images/vote-down.png';
 
 class Article extends Component {
   state = {
@@ -36,18 +40,44 @@ class Article extends Component {
     const { belongs_to, body, comments, title, username, votes } = this.state;
     return (
       <div className="article-page-container">
-        <section className="article-individual">
-          <h3>{title}</h3>
-          <p>{body}</p>
-          <p>Topic: {belongs_to}</p>
-          <p>Comment count: {comments}</p>
-          <p>Votes: {votes}</p>
-          <button onClick={() => this.handleVoteClick('up')}>Vote Up</button>
-          <button onClick={() => this.handleVoteClick('down')}>
-            Vote Down
-          </button>
-          <p>Username: {username}</p>
-        </section>
+        <div className="article-section">
+          <section className="article-individual">
+            <h3 className="h3">{title}</h3>
+            <p>{body}</p>
+            <p>
+              See more {belongs_to} articles
+              <Link className="link" to={`/topics/${belongs_to}`}>
+                <span className="span-link__topics"> here</span>
+              </Link>
+            </p>
+          </section>
+          <section className="article-info">
+            <ul className="article-info__list">
+              <li>Posted by {username}</li>
+              <li>Comments: {comments}</li>
+              <li>Votes: {votes}</li>
+            </ul>
+            <div className="vote-up-container">
+              <img
+                className="vote-up-image"
+                src={voteUp}
+                alt="vote up image"
+                onClick={() => this.handleVoteClick('up')}
+              />
+              <p>Vote up</p>
+            </div>
+            <div className="vote-down-container">
+              <img
+                className="vote-down-image"
+                src={voteDown}
+                alt="vote down image"
+                onClick={() => this.handleVoteClick('down')}
+              />
+              <p>Vote down</p>
+            </div>
+          </section>
+        </div>
+
         <section className="comments-container">
           {this.state.comments && <Comments articleId={article_id} />}
           {this.state.comments === 0 && (
@@ -60,7 +90,6 @@ class Article extends Component {
     );
   }
 
-  // optimistic rendering here, could use then block instead
   handleVoteClick = amount => {
     const { votes } = this.state;
     const article_id = this.props.match.params.article_id;
