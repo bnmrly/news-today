b859397d22d2153bec97ba336e0a73a058ae8b37
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-27 17:44:17 +0100

    conditional rendering of loading and state refactor

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as api from '../api.js';
 
-const ArticlesList = ({ articleData }) => {
-  console.log(
-    articleData,
-    'haefwtqYIKJHGSJDkuwqksadjwyhjadhgWHJ,Asdh,jhasdaj,dwha,jhg'
-  );
-  return (
-    <ul className="articles-all">
-      {articleData.map(article => {
-        return (
-          <li key={article._id}>
-            <Link className="link" to={`/articles/${article._id}`}>
-              {article.title}
-            </Link>
-            {/* <Link className="link" to={`/articles/${article._id}`}>
-              {article.title}
-            </Link> */}
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+class ArticlesList extends Component {
+  state = {
+    articleData: []
+  };
+
+  componentDidMount = () => {
+    api.fetchArticles().then(articles => {
+      this.setState({ articleData: articles });
+    });
+  };
+
+  render() {
+    const { articleData } = this.state;
+    if (!articleData.length) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <ul className="articles-all">
+          {articleData.map(article => {
+            return (
+              <li key={article._id}>
+                <Link className="link" to={`/articles/${article._id}`}>
+                  {article.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      );
+    }
+  }
+}
 
 export default ArticlesList;
